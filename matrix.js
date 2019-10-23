74a6883dc2f750b442ddeeb70bdb32cfa94d65bc
DKelly <kelly13493@gmail.com>
2019-10-22 23:29:51 -0400

    Added function to perform matrix multiplication

--- matrix.js
+++ matrix.js
@@ -27,3 +27,49 @@ function matrixToVec4(m) {
     }
     return r;
 }
+
+//the following 2 functions perform matrix mulplication
+//https://en.wikipedia.org/wiki/Matrix_multiplication
+function matmulvec(a, vec) {
+    let m = vecToMatrix(vec);
+    let r = matmul(a, m);
+    return matrixToVec(r);
+}
+
+//take a matrix that has 1 column and 4 rows and turn it into a P4Vector
+function matmulvec4(a, vec) {
+    let m = vec4ToMatrix(vec);
+    let r = matmul(a, m);
+    return matrixToVec4(r);
+}
+
+function matmul(a, b) {
+    if (b instanceof p5.Vector) {
+        return matmulvec(a, b);
+    }
+    if (b instanceof P4Vector) {
+        return matmulvec4(a, b);
+    }
+
+    let colsA = a[0].length;
+    let rowsA = a.length;
+    let colsB = b[0].length;
+    let rowsB = b.length;
+
+    if (colsA !== rowsB) {        
+        return null;
+    }
+
+    result = [];
+    for (let j = 0; j < rowsA; j++) {
+        result[j] = [];
+        for (let i = 0; i < colsB; i++) {
+            let sum = 0;
+            for (let n = 0; n < colsA; n++) {
+                sum += a[j][n] * b[n][i];
+            }
+            result[j][i] = sum;
+        }
+    }
+    return result;
+}
