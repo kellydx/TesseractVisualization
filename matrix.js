f1750bc9e16c5273e261c9a485acd37d120d9cbc
DKelly <kelly13493@gmail.com>
2019-10-22 23:26:46 -0400

    Added function to convert matrix to vector

--- matrix.js
+++ matrix.js
@@ -8,3 +8,22 @@ function vecToMatrix(v) {
     m[2][0] = v.z;
     return m;
 }
+//adding the forth dimension to draw a 4d matrix
+function vec4ToMatrix(v) {
+    let m = vecToMatrix(v);
+    m[3] = [];
+    m[3][0] = v.w;
+    return m;
+}
+
+function matrixToVec(m) {
+    return createVector(m[0][0], m[1][0], m[2][0]);
+}
+
+function matrixToVec4(m) {
+    let r = new P4Vector(m[0][0], m[1][0], m[2][0], 0);
+    if (m.length > 3) {
+        r.w = m[3][0];
+    }
+    return r;
+}
