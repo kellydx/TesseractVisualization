aae5a298fde241f1497357432262789d9e60a3e5
DKelly <kelly13493@gmail.com>
2019-10-22 23:14:49 -0400

    updating html, create 16 all vertices of the tesseract

--- sketch.js
+++ sketch.js
@@ -1,6 +1,23 @@
 function setup() {
     let size = min(windowWidth, windowHeight);
     createCanvas(size, size, WEBGL);
+    //create all 16 vertices of a tesseract
+    points[0] = new P4Vector(-1, -1, -1, 1);
+    points[1] = new P4Vector(1, -1, -1, 1);
+    points[2] = new P4Vector(1, 1, -1, 1);
+    points[3] = new P4Vector(-1, 1, -1, 1);
+    points[4] = new P4Vector(-1, -1, 1, 1);
+    points[5] = new P4Vector(1, -1, 1, 1);
+    points[6] = new P4Vector(1, 1, 1, 1);
+    points[7] = new P4Vector(-1, 1, 1, 1);
+    points[8] = new P4Vector(-1, -1, -1, -1);
+    points[9] = new P4Vector(1, -1, -1, -1);
+    points[10] = new P4Vector(1, 1, -1, -1);
+    points[11] = new P4Vector(-1, 1, -1, -1);
+    points[12] = new P4Vector(-1, -1, 1, -1);
+    points[13] = new P4Vector(1, -1, 1, -1);
+    points[14] = new P4Vector(1, 1, 1, -1);
+    points[15] = new P4Vector(-1, 1, 1, -1);
 }
 
 function draw() {
